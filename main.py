84e13104e6b99b63900a33594b1e9007064379eb
kushedow <kushedow@gmail.com>
2022-02-12 15:27:11 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+import utils
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def page_index():
+
+    candidates = utils.get_candidates_all()
+
+    page_content = utils.build_preformnatted_list(candidates)
+    return page_content
+
+
+@app.route("/skill/<skill_name>")
+def page_skill(skill_name):
+
+    candidates = utils.get_candidates_by_skill(skill_name)
+
+    page_content = utils.build_preformnatted_list(candidates)
+    return page_content
+
+
+@app.route("/candidate/<int:uid>")
+def page_candidate(uid):
+
+    candidate = utils.get_candidate_by_id(uid)
+
+    candidates = [candidate]
+    page_content = utils.build_preformnatted_list(candidates)
+    return page_content
+
+
+app.run()
---
+++ utils.py
@@ -0,0 +1,52 @@
+import json
+from pprint import pprint as pp
+
+
+def load_candidates():
+    """"""
+    with open("candidates.json", "r", encoding="utf-8") as file:
+        candidates = json.load(file)
+
+    return candidates
+
+
+def get_candidates_all():
+    """ """
+    return load_candidates()
+
+
+def get_candidates_by_skill(skill):
+    """ """
+    candidates = load_candidates()
+    skilled_candidates = []
+    skill_lower = skill.lower()
+
+    for candidate in candidates:
+        candidate_skills = candidate["skills"].lower().split(", ")
+        if skill_lower in candidate_skills:
+            skilled_candidates.append(candidate)
+
+    return skilled_candidates
+
+
+def get_candidate_by_id(uid):
+    """ """
+    candidates = load_candidates()
+    for candidate in candidates:
+        if candidate["id"] == uid:
+            return candidate
+
+
+def build_preformnatted_list(candidates):
+
+    page_content = ""
+
+    for candidate in candidates:
+        page_content += candidate["name"]+"\n"
+        page_content += candidate["position"]+"\n"
+        page_content += candidate["skills"]+"\n"
+        page_content += "\n"
+
+    return "<pre>"+page_content+"</pre>"
+
+
